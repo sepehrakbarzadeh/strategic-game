510573857b9db3651617b50f59f0a60eeea406c1
hasan barary <46402575+hasanbarary@users.noreply.github.com>
2019-01-30 16:57:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,6 @@
 from coordinates import ground
 
 
-territory = ground.Territory()
-ploc = ground.Location(0, 2)
-
-
-
-while True:
-    answer=input("Enter Your direction >>> ").lower()
-    if  answer in territory.check_possible_moves(ploc.current):
-        ploc.move(answer)
-    else:
-        print
-        continue
-    territory.draw(ploc.current)
-
+obj = Characters.factory('human')
+print(type(obj))
 
